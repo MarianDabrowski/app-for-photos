943f6ee714ef69f7a294281c906d0e1479d9103b
Marian Dabrowski <marian.dabrowski95@gmail.com>
2017-07-06 20:30:13 +0200

    last update

--- external.js
+++ external.js
@@ -16,15 +16,41 @@ var getJSON = function(url, callback) {
 };
 
 
-function createWebPage() {
-  var name = document.getElementById('searchTxt').value;
-  return webPage + "/" + name + "/";
-}
-
-
 function goToInstagramPage() {
-  var url = createWebPage();
 
-  //url.open()
-  //window.open(url, "my window", 'width=800,height=600');
+  $(function (){
+
+  var token = '5698586743.a8c7c13.4aafd409af3c432a918e352b28559468',
+      username = document.getElementById('searchTxt').value,
+      num_photos = 20;
+
+  $.ajax({
+  	url: 'https://api.instagram.com/v1/users/search',
+  	dataType: 'jsonp',
+  	type: 'GET',
+  	data: {access_token: token, q: username}, // actually it is just the search by username
+  	success: function(data){
+  		console.log(data);
+  		$.ajax({
+  			url: 'https://api.instagram.com/v1/users/' + data.data[0].id + '/media/recent',
+  			dataType: 'jsonp',
+  			type: 'GET',
+  			data: {access_token: token, count: num_photos},
+  			success: function(data2){
+  				console.log(data2);
+  				for(x in data2.data){
+  					$('ul').append('<li><img src="'+data2.data[x].images.thumbnail.url+'"></li>');
+  				}
+      			},
+  			error: function(data2){
+  				console.log(data2);
+  			}
+  		});
+  	},
+  	error: function(data){
+  		console.log(data);
+  	}
+  });
+});
+
 }
