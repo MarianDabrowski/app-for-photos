c8e249ded3398740e07e05b53b9da66cd1dc5dd0
Marian Dabrowski <marian.dabrowski95@gmail.com>
2017-07-07 12:48:53 +0200

    need id

--- external.js
+++ external.js
@@ -17,40 +17,24 @@ var getJSON = function(url, callback) {
 
 
 function goToInstagramPage() {
-
-  $(function (){
-
-  var token = '5698586743.a8c7c13.4aafd409af3c432a918e352b28559468',
+  var token = '1627612042.7622127.bb62fc4f771e47248861594a13edf5b5',// '5698586743.a8c7c13.4aafd409af3c432a918e352b28559468',
       username = document.getElementById('searchTxt').value,
       num_photos = 20;
 
-  $.ajax({
-  	url: 'https://api.instagram.com/v1/users/search',
-  	dataType: 'jsonp',
-  	type: 'GET',
-  	data: {access_token: token, q: username}, // actually it is just the search by username
-  	success: function(data){
-  		console.log(data);
-  		$.ajax({
-  			url: 'https://api.instagram.com/v1/users/' + data.data[0].id + '/media/recent',
-  			dataType: 'jsonp',
-  			type: 'GET',
-  			data: {access_token: token, count: num_photos},
-  			success: function(data2){
-  				console.log(data2);
-  				for(x in data2.data){
-  					$('ul').append('<li><img src="'+data2.data[x].images.thumbnail.url+'"></li>');
-  				}
-      			},
-  			error: function(data2){
-  				console.log(data2);
-  			}
-  		});
-  	},
-  	error: function(data){
-  		console.log(data);
-  	}
-  });
+$.ajax({
+    url: 'https://api.instagram.com/v1/users/271684459/media/recent',
+    dataType: 'jsonp',
+    type: 'GET',
+    data: {access_token: token, count: num_photos},
+    success: function(data){
+        console.log(data);
+        for( x in data.data ){
+            $('ul').append('<li><img src="'+data.data[x].images.low_resolution.url+'"></li>');
+        }
+    },
+    error: function(data){
+        console.log(data);
+    }
 });
 
 }
